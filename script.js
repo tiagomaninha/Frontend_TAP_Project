421e35a6a215d829e1e9319e203242f48025028d
koallaaa <72321725+tiagomaninha@users.noreply.github.com>
2024-11-25 01:54:07 +0000

    Header e slides atualizados

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let images = document.querySelectorAll(".image"); 
+    let slides = document.querySelectorAll(".slide"); 
     let currentSlide = 0;
-    const totalSlides = images.length - 2;
+    const totalSlides = slides.length - 1;
 
-    images.forEach((image) => {
-        image.style.transition = "none";
-        image.style.transform = `translateX(-100%)`;
+    slides.forEach((slide) => {
+        slide.style.transition = "none";
     });
 
     function moveSlide() {
-        images.forEach((image) => {
-            image.style.transition = "transform 0.5s ease-in-out";
-            image.style.transform = `translateX(${-100 * (currentSlide + 1)}%)`;
+        slides.forEach((slide) => {
+            slide.style.transition = "transform 0.5s ease-in-out";
+            slide.style.transform = `translateX(${-100 * (currentSlide)}%)`;
         });
 
         setTimeout(() => {
             if (currentSlide === totalSlides) {
-                images.forEach((image) => {
-                    image.style.transition = "none";
-                    image.style.transform = `translateX(-100%)`;
+                slides.forEach((slide) => {
+                    slide.style.transition = "none";
+                    slide.style.transform = `translateX(0%)`;
                 });
                 currentSlide = 0;
-            } else if (currentSlide === 0) {
-                images.forEach((image) => {
-                    image.style.transition = "none";
-                    image.style.transform = `translateX(-${100 * totalSlides}%)`;
-                });
-                currentSlide = totalSlides - 1;
             }
         }, 500); // transicao
     }
 
     setInterval(() => {
-        console.log(currentSlide)
         currentSlide++;
         moveSlide();
     }, 30 * 1000); // 30 segundos
+
+
+    window.onscroll = function() {
+        let precisoMudarDeImagem = window.innerWidth >= 1024 && window.innerWidth <= 1150 ? true : false;
+
+        fixarHeader()
+    };
+    
+    var header = document.querySelector("header");
+    var sticky = header.offsetTop;
+    
+    function fixarHeader() {
+        if (window.scrollY > sticky) {
+            header.classList.add("fixed");
+        } else {
+            header.classList.remove("fixed");
+        }
+    }
 });
