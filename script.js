afb73e9ffd2726d0a1f06ed375e4f863a56de5ab
Tiago Maninha <72321725+tiagomaninha@users.noreply.github.com>
2024-11-28 19:08:13 +0000

    Slide a dar 100%

--- script.js
+++ script.js
@@ -1,49 +1,88 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let slides = document.querySelectorAll(".slide"); 
-    let currentSlide = 0;
-    const totalSlides = slides.length - 1;
+// SLIDES 
+let slides = document.querySelectorAll(".slide"); 
+let botoesSlides = document.querySelectorAll(".dots-container div")
+let currentSlide = 0;
+const totalSlides = slides.length - 1;
+
+slides.forEach((slide) => {
+    slide.style.transition = "none";
+});
+
+function moveSlide() {
+    slides.forEach((slide) => {
+        slide.style.transition = "transform 0.5s ease-in-out";
+        slide.style.transform = `translateX(${-100 * (currentSlide)}%)`;
+    });
+    
+    setTimeout(() => {
+        if (currentSlide === totalSlides) {
+            slides.forEach((slide) => {
+                slide.style.transition = "none";
+                slide.style.transform = `translateX(0%)`;
+            });
+            currentSlide = 0;
+        }
+    }, 500); // transicao
+    
+}
+
+setInterval(() => {
+    let slide
+    let elementoAtivo = document.querySelector(".active")
+    
+    currentSlide++;
+    slide = currentSlide
+    moveSlide();
+
+    if (currentSlide == totalSlides) {
+        slide = 0
+    }
+    
+    elementoAtivo.classList.remove("active")
+    botoesSlides[slide].classList.toggle("active")
+}, 20 * 1000); // 30 segundos
+
+function changeSlide(n) {
+    let elementoAtivo = document.querySelector(".active")
+
+    elementoAtivo.classList.remove("active")
+    botoesSlides[n-1].classList.toggle("active")
+    currentSlide = n - 1
 
     slides.forEach((slide) => {
-        slide.style.transition = "none";
+        slide.style.transition = "transform 0.5s ease-in-out";
+        slide.style.transform = `translateX(${-100 * (n - 1)}%)`;
     });
+}
 
-    function moveSlide() {
-        slides.forEach((slide) => {
-            slide.style.transition = "transform 0.5s ease-in-out";
-            slide.style.transform = `translateX(${-100 * (currentSlide)}%)`;
-        });
+// HEADER 
 
-        setTimeout(() => {
-            if (currentSlide === totalSlides) {
-                slides.forEach((slide) => {
-                    slide.style.transition = "none";
-                    slide.style.transform = `translateX(0%)`;
-                });
-                currentSlide = 0;
-            }
-        }, 500); // transicao
-    }
+window.onscroll = function() {
+    let precisoMudarDeImagem = window.innerWidth >= 1024 && window.innerWidth <= 1150 ? true : false;
 
-    setInterval(() => {
-        currentSlide++;
-        moveSlide();
-    }, 30 * 1000); // 30 segundos
+    fixarHeader(precisoMudarDeImagem)
+};
 
+var blackLogo = document.getElementById("blackLogo");
+var whiteLogo = document.getElementById("whiteLogo");
+var header = document.querySelector("header"); 
+var sticky = header.offsetTop;
 
-    window.onscroll = function() {
-        let precisoMudarDeImagem = window.innerWidth >= 1024 && window.innerWidth <= 1150 ? true : false;
+function fixarHeader(bool) {
+    if (window.scrollY > sticky) {
+        header.classList.add("fixed");
 
-        fixarHeader()
-    };
-    
-    var header = document.querySelector("header");
-    var sticky = header.offsetTop;
-    
-    function fixarHeader() {
-        if (window.scrollY > sticky) {
-            header.classList.add("fixed");
-        } else {
-            header.classList.remove("fixed");
+        if (!bool) {
+            blackLogo.classList.add("showImage")
+            whiteLogo.classList.add("hideImage")
+        }
+    } else {
+        header.classList.remove("fixed");
+
+        
+        if (!bool) {
+            blackLogo.classList.remove("showImage")
+            whiteLogo.classList.remove("hideImage")
         }
     }
-});
+}    
